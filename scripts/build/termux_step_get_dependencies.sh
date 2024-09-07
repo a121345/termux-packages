e008773e8aba1bbc2a3a56fa55c17d67ae3c9e62
Maxython <mixython@gmail.com>
2024-09-07 19:40:40 +0300

    Test implementation of virtual packages
    Virtual packages are packages that are compiled and installed into the system, but are not assembled into a source package for package managers. One advantage of virtual packages is that they can use sources (including values from configured variables) from regular packages. This will allow us to easily create special virtual packages that will have to resolve cyclic dependencies in regular packages.
    
    PS: not ready for review yet, please wait
    
    [no ci]

--- scripts/build/termux_step_get_dependencies.sh
+++ scripts/build/termux_step_get_dependencies.sh
@@ -12,7 +12,7 @@ termux_step_get_dependencies() {
 		# Checking for duplicate dependencies
 		local cyclic_dependence=false
 		if termux_check_package_in_building_packages_list "$PKG_DIR"; then
-			echo "A circular dependency was found on '$PKG', the old version of the package will be installed to resolve the conflict"
+			echo "A circular dependency was found on $PKG, the old version of the package will be installed to resolve the conflict"
 			cyclic_dependence=true
 			if [ "$TERMUX_INSTALL_DEPS" = false ]; then
 				TERMUX_INSTALL_DEPS=true termux_download_repo_file
@@ -49,20 +49,24 @@ termux_step_get_dependencies() {
 					continue
 				fi
 				if ! TERMUX_WITHOUT_DEPVERSION_BINDING=$(test "${cyclic_dependence}" = "true" && echo "true" || echo "${TERMUX_WITHOUT_DEPVERSION_BINDING}") termux_download_deb_pac $PKG $DEP_ARCH $DEP_VERSION $DEP_VERSION_PAC; then
-					if [ "$cyclic_dependence" = "true" ] || ([ "$TERMUX_FORCE_BUILD_DEPENDENCIES" = "true" ] && [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]); then
-						echo "Download of $PKG$(test ${TERMUX_WITHOUT_DEPVERSION_BINDING} = false && test ${cyclic_dependence} = false && echo "@$DEP_VERSION") from $TERMUX_REPO_URL failed"
+					if [ "$TERMUX_FORCE_BUILD_DEPENDENCIES" = "true" ] && [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
+						echo "Download of $PKG$(test ${TERMUX_WITHOUT_DEPVERSION_BINDING} = false && echo "@$DEP_VERSION") from $TERMUX_REPO_URL failed"
 						return 1
 					else
-						echo "Download of $PKG$(test ${TERMUX_WITHOUT_DEPVERSION_BINDING} = false && echo "@$DEP_VERSION") from $TERMUX_REPO_URL failed, building instead"
+						if [ "$cyclic_dependence" = "true" ]; then
+							echo "Unable to resolve circular dependency by installing prebuilt $PKG package, building virtual package instead"
+						else
+							echo "Download of $PKG$(test ${TERMUX_WITHOUT_DEPVERSION_BINDING} = false && echo "@$DEP_VERSION") from $TERMUX_REPO_URL failed, building instead"
+						fi
 						build_dependency=true
 					fi
 				fi
 			fi
+			if $build_dependency; then
+				termux_run_build-package
+				continue
+			fi
 			if [ "$cyclic_dependence" = false ]; then
-				if $build_dependency; then
-					termux_run_build-package
-					continue
-				fi
 				termux_add_package_to_built_packages_list "$PKG"
 			fi
 			if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
@@ -132,11 +136,19 @@ termux_run_build-package() {
 			set_library="glibc"
 		fi
 	fi
+	local pkg_path="$PKG_DIR"
+	if [ "$cyclic_dependence" = "true" ]; then
+		pkg_path="virtual-packages/$(basename ${pkg_path})"
+		if [ ! -d "${pkg_path}" ]; then
+			echo "Virtual package $PKG (${pkg_path}) for resolving circular dependencies not found, impossible to solve the cyclic dependency"
+			return 1
+		fi
+	fi
 	TERMUX_BUILD_IGNORE_LOCK=true ./build-package.sh \
 		$(test "${TERMUX_INSTALL_DEPS}" = "true" && echo "-I") \
  		$(test "${TERMUX_FORCE_BUILD_DEPENDENCIES}" = "true" && echo "-F") \
    		$(test "${TERMUX_WITHOUT_DEPVERSION_BINDING}" = "true" && echo "-w") \
-     		--format $TERMUX_PACKAGE_FORMAT --library $set_library "${PKG_DIR}"
+		--format $TERMUX_PACKAGE_FORMAT --library $set_library "${pkg_path}"
 }
 
 termux_download_repo_file() {
